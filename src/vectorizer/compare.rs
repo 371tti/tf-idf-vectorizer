098f1745529cf3abe2a41863faab06a0542d4cb8
371tti <sannnanaitti@gmail.com>
2025-07-23 10:36:34 +0900

    cosine_similarityメソッドの実装を修正し、ベクトルのノルム計算を適切に行うようにしました。

--- src/vectorizer/compare.rs
+++ src/vectorizer/compare.rs
@@ -32,10 +32,16 @@ impl Compare<u16> for DefaultCompare {
     }
 
     fn cosine_similarity(vec: impl Iterator<Item = u16>, other: impl Iterator<Item = u16>) -> f64 {
-        let vec_iter = vec.iter()
-        let vec_norm = DefaultCompare::dot(vec, vec);
-        let other_norm = DefaultCompare::dot(other, other);
-        DefaultCompare::dot(vec, other) / (vec_norm * other_norm).sqrt()
+        let mut vec_dot = 0_f64;
+        let mut other_dot = 0_f64;
+        let mut dot = 0_f64;
+        for (a, b) in vec.zip(other) {
+            vec_dot += ((a as u32 * a as u32 + u32::MAX - 1) / u32::MAX) as f64;
+            other_dot += ((b as u32 * b as u32 + u32::MAX - 1) / u32::MAX) as f64;
+            dot += ((a as u32 * b as u32 + u32::MAX - 1) / u32::MAX) as f64;
+        }
+        let cos = dot / (vec_dot.sqrt() * other_dot.sqrt());
+        cos
     }
 
     fn euclidean_distance(vec: impl Iterator<Item = u16>, other: impl Iterator<Item = u16>) -> f64 {
